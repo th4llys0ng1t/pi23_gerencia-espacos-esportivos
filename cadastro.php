474ad030ba3bcd5c84d0f6194e145187ac1c9c50
diogofariasz <diogo.farias@escolar.ifrn.edu.br>
2023-10-30 11:29:12 -0300

    primeiro rascunho do cadastro

--- cadastro.php
+++ cadastro.php
@@ -5,66 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerencia Esportiva</title>
-    <link href="stylesheet" rel="style2.css">
-    <link href="stylesheet" rel="style.css">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  
+
     <style>
-        body{
-            margin: 20px;
-            background-color: #252c3a;
-            font-family: Arial, Helvetica, sans-serif;
-            overflow: hidden;
-        }
-        .area-login{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 70vh;
-            justify-content: center;
-            align-items: center;
-        }
-        .login{
-          margin-top: 50px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            background-color: #23232e;
-            border-radius: 15px;
-            width: 355px;
-            height: 420px;
-            padding: 35px;
-        }
-        .login form{
-            display: flex;
-            width: 100%;
-            flex-direction: column;
-        }
-        .login input{
-            margin-top: 10px;
-            background-color: #ffff;
-            padding-left: 10px;
-            color: black;
-            border: none;
-            height: 35px;
-            outline: none;
-            border-radius: 8px;
-        }
-        .login img{
-            width: 95px;
-            height: auto;
-        }
-        .login::placeholder{
-            color: black;
-            font-size: 14px;
-            
-        }
-    
-         #navbar{
-             position: absolute ;
-             top: 0;
-             left: 0;
-            width: 100%;
+
+body{
+    margin: 20px;
+    padding: 0;
+    font-family: 'Inter', sans-serif;
+    background-color: rgb(43, 40, 40);
+}
+
+  p{
+   
+    color: #ffff;
+  }
+  #navbar{
+    position: absolute ;
+    top: 0;
+    left: 0;
+    width: 100%;
     height: 100px;
     background-color: rgb(43, 40, 40);
   }
@@ -80,29 +42,54 @@
 .li{
   margin-right: 10px;
 }
-        form [type="submit"]{
-            display: block;
-            background-color: #0772EB;
-            font-size: 20px;
-            text-transform: uppercase;
-            font-weight: bold;
-            color: #ffff;
-            cursor: pointer;
+
+  .area-login{
+    margin-top: 30px;
+    padding: 70px;
+    background-color: #ffff;
+    justify-content: center;
+    align-items: center;
+    height: 70vh;
+    justify-content: center;
+    align-items: center;
+  }
+  .login{
+          margin-top: 50px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            border-radius: 15px;
+            width: 355px;
+            height: 420px;
+            padding: 35px;
         }
-        p{
-            color: #ffff;
+        .login form{
+            display: flex;
+            width:350px;
+           border-radius:15px;
+            flex-direction: column;
         }
-        a{
-            color: #0772EB;
-            text-decoration: none;
-            margin-left: 5px;
-        }
+  form{
+    background-color: rgb(43, 40, 40);
+  }
+  
+  form [type="submit"]{
+    background-color: #0f7a0f;
+
+  }
+  a {
+    color: #ffff;
+    text-decoration: none;
+    transition: 0.3s;
+  }
     </style>
 </head>
 <body>
-<nav class="col-md col-xl navbar navbar-expand-lg navbar-dark " id = "navbar">
+    <header>
+        
+      <nav class="col-md col-xl navbar navbar-expand-lg navbar-dark " id = "navbar">
         <div class="container-fluid">
-            <a class="logonav" href="index.php"><img src="../img/logo.png" width="90" height="auto" alt="ifrn"></a>
+            <a class="logonav" href="index.php"><img src="img/logo.png" width="90" height="auto" alt="ifrn"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -129,27 +116,47 @@
           </div>
         </div>
       </nav>
-    <section class="area-login">
+       
+       <center>
+      <section class="area-login">
         <div class="login">
             <div>
-                <img src="imagens/logo2_1.png">
+                <img src="img/logo.png" width="100">
             </div>
             <form>
                
-                <input type="text" name="nome" placeholder="Email" autofocus>
-                <input type="text" name="nome" placeholder="Confirme seu email" autofocus>
+                <input type="text" name="nome" placeholder="Email Escolar" autofocus>
+                <input type="text" name="nome" placeholder="Matrícula" autofocus>
                 <input type="password" name="senha" placeholder="Senha" autofocus>
                 <input type="password" name="senha" placeholder="Confirme sua senha" autofocus>
                 <input type="submit" value="entrar">
             </form>
            
         </div>
+        </center>
+    </div>
 
-    </section>
-    <footer>
-        <br/>
-          <p><center>Copyright &copy Hermes Bookstore - IFRN Cang 2023</center></p>
-      </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <footer class="main_footer container">
+    <!--content-->
+    <div class="content">
+    
+	<p> Copyright &copy Gerencia Esportiva - IFRN Cang 2023</p>
+	<nav>
+		<ul>
+			<li><a href="#">Termos de uso</a></li>
+			<li><a href="#">Política de privacidade</a></li>
+			<li><a href="#">Sobre nós</a></li>
+			<li><a href="#">Contato</a></li>
+		</ul>
+	</nav>
+    
+    </div><!--Contant-->
+    
+          
+</footer>
+ 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+       
 </body>
 </html>
