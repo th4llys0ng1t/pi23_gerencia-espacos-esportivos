a8d09334e63a2297409ce17de6b7e401223944a0
guilhermedl12 <g.dionizio@escolar.ifrn.edu.br>
2023-11-10 14:24:32 -0300

    codigo de perfil com forms comentado

--- perfil.php
+++ perfil.php
@@ -75,7 +75,7 @@ a {
 }
 .wrapp{
   display: grid;
-  grid-template-columns: 200  px 1fr;
+  grid-template-columns: 200px 1fr;
 }
 </style>
 </head>
@@ -122,6 +122,27 @@ a {
       <a href="#" class="list-group-item list-group-item-action">Configurações</a>
     </div>
   </div>
+    <!-- <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInputDisabled" placeholder="name@example.com" disabled>
+  <label for="floatingInputDisabled">Email address</label>
+</div>
+<div class="form-floating mb-3">
+  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextareaDisabled" disabled></textarea>
+  <label for="floatingTextareaDisabled">Comments</label>
+</div>
+<div class="form-floating mb-3">
+  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2Disabled" style="height: 100px" disabled>Disabled textarea with some text inside</textarea>
+  <label for="floatingTextarea2Disabled">Comments</label>
+</div>
+<div class="form-floating">
+  <select class="form-select" id="floatingSelectDisabled" aria-label="Floating label disabled select example" disabled>
+    <option selected>Open this select menu</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
+  <label for="floatingSelectDisabled">Works with selects</label>
+</div>-->
   <br>
   <br>
   <br>
