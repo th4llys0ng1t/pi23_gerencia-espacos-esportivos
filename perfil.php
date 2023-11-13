a4f7ae752af1899c3476a26dbcb6177e714943b2
Marquinhos <marcos9608antonio@gmail.com>
2023-11-13 01:05:35 -0300

    mexi no perfil dnv

--- perfil.php
+++ perfil.php
@@ -1,172 +1,87 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-  <title>Perfil</title>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
-  <style>  
-  a {
-    color: #ffff;
-    text-decoration: none;
-    transition: 0.3s;
-  }
-  li {
-	display: inline-block;
-	margin: 0 10px;
-}
-  .content {
-	background-color:  #1f2124;
-	color: #ffff;
-	padding: 20px;
-	text-align: center;
-	margin-top: 50px;
-}
-body{
-    margin: 20px;
-    padding: 0;
-    font-family: 'Inter', sans-serif;
-    background-color: rgb(43, 40, 40);
-}
-
-  p{
-   
-    color: #ffff;
-  }
-  
-  h2{
-   color: #ffff;
-  }
-  form [type="submit"]{
-    background-color: #0f7a0f;
-  }
-  .content {
-    background-color:  rgb(43, 40, 40);
-    background-size: cover;
-    color: #fff;
-    padding: 20px;
-    text-align: center;
-    margin-top: 50px;
-    position: fixed;
-    bottom:0;
-    left:0;
-    width: 100%;
-           
-}
-
-ul {
-	list-style: none;
-	padding: 0;
-	margin: 0;
-}
-
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
-
-a {
-	color: #fff;
-	text-decoration: none;
-}
-.wrapp{
-  display: grid;
-  grid-template-columns: 200px 1fr;
-}
-</style>
+  <title>Responsive Sidebar</title>
+  <!-- Link Styles -->
+  <link rel="stylesheet" href="estilo.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
-
-<nav class="col-md col-xl navbar navbar-expand-lg navbar-dark " id = "navbar">
-    <div class="container-fluid">
-        <a class="logonav" href="index.php"><img src="img/logo.png" width="90" height="auto" alt="ifrn"></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item" style = "margin: 20px;">
-            <a class="nav-link" href="#">ÁREAS ESPORTIVAS</a>
-          </li>
-          <li class="nav-item" style = "margin: 20px;">
-            <a class="nav-link" href="#">CADASTRO</a>
-          </li>
-          <li class="nav-item" style = "margin: 20px;">
-            <a class="nav-link" href="#">LOGIN</a>
-          </li>
-          <li class="nav-item" style = "margin: 20px;">
-            <a class="nav-link" href="#">PERFIL</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Digite sua busca" aria-label="Search">
-          <button class="btn btn-dark" type="submit">Buscar</button>
-        </form>
-        
-      </div>
+<body>
+  <div class="sidebar">
+    <div class="logo_details">
+    <a class="logonav" href="index.php"><img src="img/logo.png" width="30" height="auto" alt="ifrn"></a>
+      <div class="logo_name">IFRN|CANG</div>
+      <i class="bx bx-menu" id="btn"></i>
     </div>
-  </nav>
-  <br>
-  <h2>
-  <p>Perfil | Nome do usuário</p>
-  </h2>
-  <br>
-  <div class="wrapp">
-    <div class="list-group">
-      <a href="#" class="list-group-item list-group-item-action active" aria-current="true">Início</a>
-      <a href="#" class="list-group-item list-group-item-action">Suas reservas</a>
-      <a href="#" class="list-group-item list-group-item-action">Favoritadas</a>
-      <a href="#" class="list-group-item list-group-item-action">Configurações</a>
-    </div>
+    <ul class="nav-list">
+      <li>
+        <i class="bx bx-search"></i>
+        <input type="text" placeholder="Search...">
+         <span class="tooltip">Search</span>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bx-grid-alt"></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+        <span class="tooltip">Dashboard</span>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bx-user"></i>
+          <span class="link_name">User</span>
+        </a>
+        <span class="tooltip">User</span>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bx-chat"></i>
+          <span class="link_name">Message</span>
+        </a>
+        <span class="tooltip">Message</span>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bx-pie-chart-alt-2"></i>
+          <span class="link_name">Analytics</span>
+        </a>
+        <span class="tooltip">Analytics</span>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bx-folder"></i>
+          <span class="link_name">File Manger</span>
+        </a>
+        <span class="tooltip">File Manger</span>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bx-cart-alt"></i>
+          <span class="link_name">Order</span>
+        </a>
+        <span class="tooltip">Order</span>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bx bx-cog"></i>
+          <span class="link_name">Settings</span>
+        </a>
+        <span class="tooltip">Settings</span>
+      </li>
+      <li class="profile">
+        <div class="profile_details">
+        </div>
+        <i class="bx bx-log-out" id="log_out"></i>
+      </li>
+    </ul>
   </div>
-    <!-- <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInputDisabled" placeholder="name@example.com" disabled>
-  <label for="floatingInputDisabled">Email address</label>
-</div>
-<div class="form-floating mb-3">
-  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextareaDisabled" disabled></textarea>
-  <label for="floatingTextareaDisabled">Comments</label>
-</div>
-<div class="form-floating mb-3">
-  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2Disabled" style="height: 100px" disabled>Disabled textarea with some text inside</textarea>
-  <label for="floatingTextarea2Disabled">Comments</label>
-</div>
-<div class="form-floating">
-  <select class="form-select" id="floatingSelectDisabled" aria-label="Floating label disabled select example" disabled>
-    <option selected>Open this select menu</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-  </select>
-  <label for="floatingSelectDisabled">Works with selects</label>
-</div>-->
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <section class="">
-    <!-- Footer -->
-    <footer class="main_footer container">
+  <section class="home-section">
+    <div class="text">PERFIL|IFRN</div>
+  </section>
+  <!-- Scripts -->
+  <script src="java.js"></script>
+
+     <footer class="main_footer container">
     <!--content-->
     <div class="content">
     
@@ -184,7 +99,6 @@ a {
     
           
 </footer>
-    <!-- Footer -->
-  </section>
+ 
 </body>
 </html>
