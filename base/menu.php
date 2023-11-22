c60e489b4277d25f2b49d47143d44ea02eb4a737
Paulo César Dias Rufino <paulocesar04122004@gmail.com>
2023-11-22 13:54:45 -0300

    rojgr

---
+++ base/menu.php
@@ -0,0 +1,29 @@
+<nav class="col-md col-xl navbar navbar-expand-lg navbar-dark " id = "navbar">
+        <div class="container-fluid">
+            <a class="logonav" href="index.php"><img src="../src/IMG/logo.png" width="90" height="auto" alt="ifrn"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item" style = "margin: 20px;">
+                <a class="nav-link" href="index.php">ÁREAS ESPORTIVAS</a>
+              </li>
+              <li class="nav-item" style = "margin: 20px;">
+                <a class="nav-link" href="cadastro.php">CADASTRO</a>
+              </li>
+              <li class="nav-item" style = "margin: 20px;">
+                <a class="nav-link" href="autenticacao.php">LOGIN</a>
+              </li>
+              <li class="nav-item" style = "margin: 20px;">
+                <a class="nav-link" href="perfil.php">PERFIL</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Digite sua busca" aria-label="Search">
+              <button class="btn btn-dark" type="submit">Buscar</button>
+            </form>
+            
+          </div>
+        </div>
+      </nav>
