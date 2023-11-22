5bd5f109879a6422fc89bac703e246b1ee209e14
Paulo César Dias Rufino <paulocesar04122004@gmail.com>
2023-11-22 14:09:58 -0300

    ffgr

--- php/index.php
+++ php/index.php
@@ -8,15 +8,19 @@
     <link rel="stylesheet" href="../src/CSS/style2.css">
     <link rel="stylesheet" href="../src/CSS/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
     <style>
+
       .card {
         background-color: #0f7a0f;
         color: #ffff;
       }
+        
     </style>
 </head>
 <body>
     <header>
+        
       <nav class="col-md col-xl navbar navbar-expand-lg navbar-dark " id = "navbar">
         <div class="container-fluid">
             <a class="logonav" href="index.php"><img src="../src/IMG/logo.png" width="90" height="auto" alt="ifrn"></a>
@@ -42,134 +46,141 @@
               <input class="form-control me-2" type="search" placeholder="Digite sua busca" aria-label="Search">
               <button class="btn btn-dark" type="submit">Buscar</button>
             </form>
+            
           </div>
         </div>
       </nav>
-
-      <center>
+       
+       
+       <center>
         <div class="row">
-          <div class="col-md-6 col-xl-3" >
-            <a href="#">
-            <div class="card" style="width: 18rem;">
-              <img src="../src/IMG/campus.jpg" class="card-img-top" alt="quadra">
-            </div>
+
+        <div class="col-md-6 col-xl-3" >
+          <a href="#">
+          <div class="card" style="width: 18rem;">
+          <img src="../src/IMG/campus.jpg" class="card-img-top" alt="quadra">
+          <div class="card-body">
+          </a>  <p class="card-text"><center>GÍNASIO</center></p>
+          </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-xl-3" >
+          <a href="#">
+          <div class="card" style="width: 18rem;">
+            <img src="../src/IMG/campus.jpg" class="card-img-top" alt="volei">
             <div class="card-body">
-              </a><p class="card-text"><center>GÍNASIO</center></p>
+            </a> <p class="card-text"><center>QUADRA DE AREIA</center></p>
             </div>
           </div>
+        </div>
 
-          <div class="col-md-6 col-xl-3" >
-            <a href="#">
-            <div class="card" style="width: 18rem;">
-              <img src="../src/IMG/campus.jpg" class="card-img-top" alt="volei">
-              <div class="card-body">
-                </a> <p class="card-text"><center>QUADRA DE AREIA</center></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6 col-xl-3" >
-            <a href="#">
+        <div class="col-md-6 col-xl-3" >
+          <a href="#">
             <div class="card" style="width: 18rem;">
               <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
               <div class="card-body">
-                </a> <p class="card-text"><center>PISCINA</center></p>        
+              </a> <p class="card-text"><center>PISCINA</center></p>        
               </div>  
             </div>
           </div>
 
           <div class="col-md-6 col-xl-3" >
             <a href="#">
-            <div class="card" style="width: 18rem;">
-              <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+              <div class="card" style="width: 18rem;">
+                <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+                <div class="card-body">
+                </a> <p class="card-text"><center>SALA DE JOGOS</center></p>        
+                </div>  
+              </div>
             </div>
-            <div class="card-body">
-                  </a> <p class="card-text"><center>SALA DE JOGOS</center></p>        
-            </div>  
-          </div>
 
-          <div class="col-md-6 col-xl-3" >
+            <div class="col-md-6 col-xl-3" >
             <a href="#">
-            <div class="card" style="width: 18rem;">
-              <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+              <div class="card" style="width: 18rem;">
+                <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+                <div class="card-body">
+                </a> <p class="card-text"><center>SALA DE MÚSICA</center></p>        
+                </div>  
+              </div>
             </div>
-            <div class="card-body">
-              </a> <p class="card-text"><center>SALA DE MÚSICA</center></p>        
-            </div>  
-          </div>
 
-          <div class="col-md-6 col-xl-3" >
+            <div class="col-md-6 col-xl-3" >
             <a href="#">
-            <div class="card" style="width: 18rem;">
-              <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+              <div class="card" style="width: 18rem;">
+                <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+                <div class="card-body">
+                </a> <p class="card-text"><center>ACADEMIA</center></p>        
+                </div>  
+              </div>
             </div>
-            <div class="card-body">
-              </a><p class="card-text"><center>ACADEMIA</center></p>        
-            </div>  
-          </div>
 
-          <div class="col-md-6 col-xl-3" >
+            <div class="col-md-6 col-xl-3" >
             <a href="#">
-            <div class="card" style="width: 18rem;">
-              <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+              <div class="card" style="width: 18rem;">
+                <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+                <div class="card-body">
+                </a> <p class="card-text"><center>SALA DE LUTAS</center></p>        
+                </div>  
+              </div>
             </div>
-            <div class="card-body">
-              </a><p class="card-text"><center>SALA DE LUTAS</center></p>        
-            </div>  
-          </div>
 
-          <div class="col-md-6 col-xl-3" >
+            <div class="col-md-6 col-xl-3" >
             <a href="#">
-            <div class="card" style="width: 18rem;">
-              <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+              <div class="card" style="width: 18rem;">
+                <img src="../src/IMG/campus.jpg" class="card-img-top" alt="piscina">
+                <div class="card-body">
+                </a> <p class="card-text"><center>SALA DE ARTES</center></p>        
+                </div>  
+              </div>
             </div>
-            <div class="card-body">
-              </a><p class="card-text"><center>SALA DE ARTES</center></p>        
-            </div>  
-          </div>
         </div>
-      </center>
+    </center>
     
     <footer class="rodape" id="contato">
-      <div class="rodape-div">
+    <div class="rodape-div">
+
         <div class="rodape-div-1">
-          <div class="rodape-div-1-coluna">
-            <!-- elemento -->
-            <span><a class="logo" href="index.php"><img src="../src/IMG/logo.png" width="70" height="auto" alt="ifrn"></a></span>
-            <p>160 BR-101, S/N - Areia Branca, </br>Canguaretama - RN, 59190-000</p>
-          </div>
+            <div class="rodape-div-1-coluna">
+                <!-- elemento -->
+                <span><a class="logo" href="index.php"><img src="../src/IMG/logo.png" width="70" height="auto" alt="ifrn"></a></span>
+                <p>160 BR-101, S/N - Areia Branca, </br>Canguaretama - RN, 59190-000</p>
+            </div>
         </div>
 
         <div class="rodape-div-2">
-          <div class="rodape-div-2-coluna">
-            <!-- elemento -->
-            <span><b>Contatos</b></span>
-            <p>ifrn@gmail.com</p>
-            <p>+55 (84) 99999-9999</p>
-          </div>
+            <div class="rodape-div-2-coluna">
+                <!-- elemento -->
+                <span><b>Contatos</b></span>
+                <p>ifrn@gmail.com</p>
+                <p>+55 (84) 99999-9999</p>
+            </div>
         </div>
 
         <div class="rodape-div-3">
-          <div class="rodape-div-3-coluna">
-            <!-- elemento -->
-            <span><b>Links</b></span>
-            <p><a href="https://portal.ifrn.edu.br/servicos/">Serviços</a></p>
-            <p><a href="https://portal.ifrn.edu.br/campus/canguaretama/">Instituto</a></p>
-            <p><a href="https://portal.ifrn.edu.br/">Sobre</a></p>
-          </div>
+            <div class="rodape-div-3-coluna">
+                <!-- elemento -->
+                <span><b>Links</b></span>
+                <p><a href="https://portal.ifrn.edu.br/servicos/">Serviços</a></p>
+                <p><a href="https://portal.ifrn.edu.br/campus/canguaretama/">Instituto</a></p>
+                <p><a href="https://portal.ifrn.edu.br/">Sobre</a></p>
+            </div>
         </div>
 
         <div class="rodape-div-4">
-          <div class="rodape-div-4-coluna">
-              <!-- elemento -->
-              <span><b>Outros</b></span>
-              <p>Políticas de Privacidade</p>
-          </div>
+            <div class="rodape-div-4-coluna">
+                <!-- elemento -->
+                <span><b>Outros</b></span>
+                <p>Políticas de Privacidade</p>
+            </div>
         </div>
-      </div>
-      <p class="rodape-direitos">Copyright © 2023 – Todos os Direitos Reservados ao IFRN.</p>
-    </footer>
+
+    </div>
+    <p class="rodape-direitos">Copyright © 2023 – Todos os Direitos Reservados ao IFRN.</p>
+</footer>
  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+       
 </body>
 </html>
--- php/perfil.php
+++ php/perfil.php
@@ -3,121 +3,18 @@
 <head>
   <title>Gerencia Esportiva</title>
   <!-- Link Styles -->
-  <link rel="stylesheet" href="../src/CSS/estilo.css">
-  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="../src/CSS/style2.css">
+    
+  <?php include '../base/head.php'?>
 </head>
 <body>
-  <div class="sidebar">
-    <div class="logo_details">
-      <a class="logonav" href="index.php"><img src="../src/IMG/logo.png" width="30" height="auto" alt="ifrn"></a>
-      <div class="logo_name">IFRN|CANG</div>
-      <i class="bx bx-menu" id="btn"></i>
-    </div>
-    <ul class="nav-list">
-      <li>
-        <i class="bx bx-search"></i>
-        <input type="text" placeholder="Search...">
-         <span class="tooltip">Search</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class="bx bx-grid-alt"></i>
-          <span class="link_name">Dashboard</span>
-        </a>
-        <span class="tooltip">Dashboard</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class="bx bx-user"></i>
-          <span class="link_name">User</span>
-        </a>
-        <span class="tooltip">User</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class="bx bx-chat"></i>
-          <span class="link_name">Message</span>
-        </a>
-        <span class="tooltip">Message</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class="bx bx-pie-chart-alt-2"></i>
-          <span class="link_name">Analytics</span>
-        </a>
-        <span class="tooltip">Analytics</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class="bx bx-folder"></i>
-          <span class="link_name">File Manger</span>
-        </a>
-        <span class="tooltip">File Manger</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class="bx bx-cart-alt"></i>
-          <span class="link_name">Order</span>
-        </a>
-        <span class="tooltip">Order</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class="bx bx-cog"></i>
-          <span class="link_name">Settings</span>
-        </a>
-        <span class="tooltip">Settings</span>
-      </li>
-      <li class="profile">
-        <div class="profile_details">
-        </div>
-        <i class="bx bx-log-out" id="log_out"></i>
-      </li>
-    </ul>
-  </div>
-  <section class="home-section">
-    <div class="text">PERFIL|IFRN</div>
-  </section>
-  <!-- Scripts -->
-  <script src="java.js"></script>
-  <footer class="rodape" id="contato">
-    <div class="rodape-div">
-      <div class="rodape-div-1">
-        <div class="rodape-div-1-coluna">
-          <!-- elemento -->
-          <span><b>LOGO</b></span>
-          <p>SIA Trecho 5 lote 000 bloco z sala 900 - Guará, Brasília - DF, 70000-010</p>
-        </div>
-      </div>
-
-      <div class="rodape-div-2">
-        <div class="rodape-div-2-coluna">
-          <!-- elemento -->
-          <span><b>Contatos</b></span>
-          <p>contato@na.na</p>
-          <p>+55 63 99200-0000</p>
-        </div>
-      </div>
-
-      <div class="rodape-div-3">
-        <div class="rodape-div-3-coluna">
-          <!-- elemento -->
-          <span><b>Links</b></span>
-          <p><a href="#servicos">Serviços</a></p>
-          <p><a href="#empresa">Empresa</a></p>
-          <p><a href="#sobre">Sobre</a></p>
-        </div>
-      </div>
-
-      <div class="rodape-div-4">
-        <div class="rodape-div-4-coluna">
-          <!-- elemento -->
-          <span><b>Outros</b></span>
-          <p>Políticas de Privacidade</p>
-        </div>
-      </div>
-    </div>
-    <p class="rodape-direitos">Copyright © 2023 – Todos os Direitos Reservados.</p>
-  </footer>
+<?php include '../base/menu.php'?>
+<br>
+<br><br><br><br><br><br><br><br><br>
+<br>
+<br><br><br><br><br><br><br><br><br>
+<br>
+<br><br><br><br><br><br><br>
+  <?php include '../base/rodape.php'?>
 </body>
 </html>
