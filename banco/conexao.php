96402222ac634a0eb87dfa7f8a0f8b5698bf4ab5
Marquinhos <marcos9608antonio@gmail.com>
2023-11-22 14:09:50 -0300

    concluido

---
+++ banco/conexao.php
@@ -0,0 +1,21 @@
+<?php
+
+function conectar()
+{
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha ="123";
+    $banco = "projeto_gerenciamento_espaços_esportivos";
+
+    $conn = new mysqli($servidor, $usuario, $senha, $banco);
+
+    if ($conn->connect_error) {
+        die("a conexão falhou!" . $conn->connect_error);
+    }
+
+    return $conn;
+}
+
+function desconectar($conn){
+    $conn->close();
+}
