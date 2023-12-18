eb02bc02b34f88690bd487f3683518a09b8d4f43
Thallyson Kauã <thallysonvidal566@gmail.com>
2023-12-18 10:15:12 -0300

    organizando o código de funcionamento da página de autenticação e do cadastro.

--- php_bd/autenticacao_bd.php
+++ php_bd/autenticacao_bd.php
@@ -1,17 +1,26 @@
 <?php 
 
+    //recolhendo os dados digitados pelo usuário
     $matricula=$_POST['matricula'];
     $senha=$_POST['senha'];
+
+    //criptando a senha
     $senha_cripto=md5($senha);
+
+    //incluindo a conexão com o banco de dados
     include "../banco/conexao.php";
+
     $conn=conectar();
+
+    //SQL de inserção no banco
     $sql="SELECT * FROM administradores WHERE MATRICULA = '$matricula' AND SENHA = '$senha_cripto'";
     $resultado=$conn -> query($sql);
+
+    //teste de acerto ou erro
     if ($resultado -> num_rows > 0) {
         while ($row=$resultado -> fetch_assoc) {
             session_start();
             $_SESSION["matricula"]=$row ["USUARIO"];
-
         }
         desconectar($conn);
         header("Location: ../perfil.php");
--- php_bd/cadastro_bd.php
+++ php_bd/cadastro_bd.php
@@ -1,14 +1,26 @@
 <?php 
+
+    //recolhendo os dados do usuário
     $usuario=$_POST['usuario'];
     $email=$_POST['email'];
     $senha=$_POST['senha'];
     $matricula=$_POST['matricula'];
+
+    //criptando a senha
     $senha_cripto=md5($senha);
+
+    //incluindo a conexão com o banco de dados
     include "../banco/conexao.php";
+
     $conn=conectar();
+
+    //SQL de inserção no banco de dados
     $sql = "INSERT INTO administrador (EMAIL, MATRICULA, USUARIO, SENHA)
     VALUES ('$email', '$matricula',' $usuario','$senha_cripto')";
+
     $resultado=$conn -> query($sql);
+
+    //teste de erro o acerto
     if ($resultado) {
         desconectar($conn);
         header ("Location: ../autenticacao.php");
