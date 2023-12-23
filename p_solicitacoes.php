b5bc4ebe716a0a969802a6303914ba03b5b37271
unknown <ribeiro.neves@escolar.ifrn.edu.br>
2023-12-23 00:12:03 -0300

    crud

---
+++ p_solicitacoes.php
@@ -0,0 +1,24 @@
+<?php include 'sessao/verificar_autenticado.php'; ?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title> Solicitações </title> 
+    <?php include "base/head.php"?>
+    <link rel="stylesheet" href="src/CSS/style2.css">
+    <link rel="stylesheet" href="src/CSS/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .incio{
+          margin-top: 10%;
+          margin-bottom: 9%;
+      }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ p_solicitacoes_adicionar.php
@@ -0,0 +1,1 @@
+<?php include 'sessao/verificar_autenticado.php'; ?>
