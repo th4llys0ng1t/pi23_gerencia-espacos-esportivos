478ca5c22728733dd73f440b3227f9d8560f45cc
Thallyson Kauã <thallysonvidal566@gmail.com>
2023-12-30 08:31:25 -0300

    adicionando a funcionalidade de remoção das solicitações

--- php_bd/p_solicitacoes_remover_bd.php
+++ php_bd/p_solicitacoes_remover_bd.php
@@ -1,34 +1,21 @@
-<<<<<<< HEAD
 <?php 
 
-    
+    $id = $_GET["id"];
 
+    include "../banco/conexao.php";
 
+    $conn = conectar();
 
+    $sql = "DELETE FROM solicitacoes WHERE ID=$id;";
+    $resultado = $conn->query($sql);
 
-
+    if($resultado){
+        desconectar($conn);
+        header("Location: ../p_solicitacoes.php");
+        die();
+    }else{
+        desconectar($conn);
+        echo "<p>Problema</p>";
+    }
 
 ?>
-=======
-<?php
-
-$id = $_GET["id"];
-
-include "../banco/conexao.php";
-
-$conn = conectar();  
-
-$sql = "DELETE FROM espacos WHERE ID=$id;"; 
-
-$resultado = $conn->query($sql); 
-
-if($resultado){
-    desconectar($conn);
-    header("Location: ../p_solicitacoes.php");
-    die();
-
-}else{
-    desconectar($conn);
-    echo "<p>Problema</p>";
-}
->>>>>>> 822db47f38d61cfce8c59dd1a857ee14eec411b8
